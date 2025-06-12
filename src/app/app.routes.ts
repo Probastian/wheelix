7e1a55b2cd0ecb0b49982e3d3b463a6890707002
Sebastian Roßberger <sebastian.rossberger@consol.de>
2025-06-13 00:10:23 +0200

    routes and style fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {ToeComponent} from './components/modes/toe/toe.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'toe', pathMatch: 'full' },
+  { path: 'toe', component: ToeComponent },
+];
